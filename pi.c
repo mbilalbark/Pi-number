42bbabee0062455577a21635472d45b004e4a6c3
Muhammed Bilal Bark <muh.bark@gmail.com>
2017-07-16 17:32:28 +0300

    Update pi.c

--- pi.c
+++ pi.c
@@ -2,36 +2,23 @@
 
 int main()
 {
-	FILE *fp;
-     int count[10]={0,0,0,0,0,0,0,0,0};
+     FILE *fp;
+     int numbers[10]={0,1,2,3,4,5,6,7,8,9};
+     int count[10]={0};
      int a=0;
-      int c;
+     int c;
      fp = fopen("pi7.txt","r");  // burada pi dosyalarını değiştirebilirsiniz. pi den başlayıp pi 7 ye kadar gider.
      if (fp == NULL) printf("Boyle bir dosya yok\n");
+	 c= fgetc(fp);
 	do {
-       c = getc(fp);    
-       
-	     if(c=='0')
-	      count[0]++;    
-		 else if(c=='1')
-		 count[1]++;
-		 else if(c=='2')
-		 count[2]++;  
-         else if(c=='3')
-		 count[3]++; 
-		 else if(c=='4')
-		  count[4]++; 
-         else if(c=='5')
-		  count[5]++; 
-		 else if(c=='6')
-		  count[6]++;   
-         else if(c=='7')
-		  count[7]++; 
-		 else if(c=='8')
-		  count[8]++; 
-		 else if(c=='9')
-		  count[9]++; 
-		 
+       if(c==' ')
+	    c= fgetc(fp);
+	
+	else{
+	        num = atoi(&c);
+	        count[numbers[num]]++;
+	}    
+           c= fgetc(fp);
 		
 	 } while (c!=EOF);
 	 for(int i=0;i<10;i++)
