df455eb561cfe303b7c9d25055e36cdf85e38022
Muhammed Bilal Bark <muh.bark@gmail.com>
2017-07-16 17:35:27 +0300

    Update pi.c

--- pi.c
+++ pi.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+#include <stdlib.h>
 
 int main()
 {
      FILE *fp;
      int numbers[10]={0,1,2,3,4,5,6,7,8,9};
      int count[10]={0};
-     int a=0;
-     int c;
-     fp = fopen("pi7.txt","r");  // burada pi dosyalarını değiştirebilirsiniz. pi den başlayıp pi 7 ye kadar gider.
+     int a=0,num;
+     char c;
+     fp = fopen("pi.txt","r");  // burada pi dosyalarını değiştirebilirsiniz. pi den başlayıp pi 5'e kadar gider.
      if (fp == NULL) printf("Boyle bir dosya yok\n");
 	 c= fgetc(fp);
 	do {
