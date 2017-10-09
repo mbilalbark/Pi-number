2995baa0ab3291ab0fd08dd5d7af372044730f0b
Muhammed Bilal Bark <muh.bark@gmail.com>
2017-10-09 19:17:33 +0300

    Update pi.c

--- pi.c
+++ pi.c
@@ -11,20 +11,17 @@ int main()
      fp = fopen("pi.txt","r");  // burada pi dosyalarını değiştirebilirsiniz. pi den başlayıp pi 5'e kadar gider.
      if (fp == NULL) printf("Boyle bir dosya yok\n");
 	 c= fgetc(fp);
-	do {
+     do {
        if(c==' ')
-	    c= fgetc(fp);
-	
+	  c= fgetc(fp);
 	else{
-	        num = atoi(&c);
-	        count[numbers[num]]++;
+	    num = atoi(&c);
+	    count[numbers[num]]++;
 	}    
-           c= fgetc(fp);
-		
-	 } while (c!=EOF);
-	 for(int i=0;i<10;i++)
+          c= fgetc(fp);	
+	}while (c!=EOF);
+	 
+	for(int i=0;i<10;i++)
 	 printf( " count %d =%d \n",i,count[i]);
-	
-	
 	return 0;
 }
